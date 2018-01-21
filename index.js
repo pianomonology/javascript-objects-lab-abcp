f271d1fd14c9b7271d507612d86e55a9314dd447
Snel <snelson10@sfccmo.edu>
2018-01-21 22:00:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function updateObjectWithKeyAndValue (object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
-   object[key] = value;
+  object[key] = value;
   return object;
+}
+
+function deleteFromObjectByKey (object, key){
+  
 }
